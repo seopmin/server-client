0470b5d856b8467129a05a9139bb8628e0061b2e
pbit <alstjqrhkr2018@gmail.com>
2024-02-01 19:32:41 +0900

    [FEAT] server-client connect

--- src/lib/net_message.hpp
+++ src/lib/net_message.hpp
@@ -1,59 +1,120 @@
 #pragma once
 #include "net_common.hpp"
 
-namespace olc 
+namespace olc
 {
-  namespace net 
-  {
-    template <typename T>
-    struct message_header 
-    {
-      T id{};
-      uint32_t size = 0;
-    };
+	namespace net
+	{
+		///[OLC_HEADERIFYIER] START "MESSAGE"
 
-    template <typename T>
-    struct message 
-    {
-      message_header<T> header{};
-      std::vector<uint8_t> body;
+		// Message Header is sent at start of all messages. The template allows us
+		// to use "enum class" to ensure that the messages are valid at compile time
+		template <typename T>
+		struct message_header
+		{
+			T id{};
+			uint32_t size = 0;
+		};
 
-      size_t size() const {
-        return sizeof(message_header<T>) + body.size();
-      }
+		// Message Body contains a header and a std::vector, containing raw bytes
+		// of infomation. This way the message can be variable length, but the size
+		// in the header must be updated.
+		template <typename T>
+		struct message
+		{
+			// Header & Body vector
+			message_header<T> header{};
+			std::vector<uint8_t> body;
 
-      friend std::ostream& operator << (std::ostream& os, const message<T>& msg) {
-        os << "ID:" << int(msg.header.id) << " Size:" << msg.header.size;
-        return os;
-      }
+			// returns size of entire message packet in bytes
+			size_t size() const
+			{
+				return body.size();
+			}
 
-      template<typename DataType>
-      friend message<T>& operator << (message<T>& msg, const DataType& data) {
-        static_assert(std::is_standard_layout<DataType>::value, "Data is too complex to be pushed into vetor");
+			// Override for std::cout compatibility - produces friendly description of message
+			friend std::ostream& operator << (std::ostream& os, const message<T>& msg)
+			{
+				os << "ID:" << int(msg.header.id) << " Size:" << msg.header.size;
+				return os;
+			}
 
-        size_t i = msg.body.size();
+			// Convenience Operator overloads - These allow us to add and remove stuff from
+			// the body vector as if it were a stack, so First in, Last Out. These are a 
+			// template in itself, because we dont know what data type the user is pushing or 
+			// popping, so lets allow them all. NOTE: It assumes the data type is fundamentally
+			// Plain Old Data (POD). TLDR: Serialise & Deserialise into/from a vector
 
-        msg.body.resize(msg.body.size() + sizeof(DataType));
+			// Pushes any POD-like data into the message buffer
+			template<typename DataType>
+			friend message<T>& operator << (message<T>& msg, const DataType& data)
+			{
+				// Check that the type of the data being pushed is trivially copyable
+				static_assert(std::is_standard_layout<DataType>::value, "Data is too complex to be pushed into vector");
 
-        std::memcpy(msg.body.data() + i, &data, sizeof(DataType));
+				// Cache current size of vector, as this will be the point we insert the data
+				size_t i = msg.body.size();
 
-        msg.header.size = msg.size();
-        
-        return msg;
-      }
+				// Resize the vector by the size of the data being pushed
+				msg.body.resize(msg.body.size() + sizeof(DataType));
 
-      template<typename DataType>
-      friend message<T>& operator >> (message<T>& msg, DataType& data) {
-        static_assert(std::is_standard_layout<DataType>::value, "Data is too comples to be pushed into vector");
+				// Physically copy the data into the newly allocated vector space
+				std::memcpy(msg.body.data() + i, &data, sizeof(DataType));
 
-        size_t i = msg.body.size() - sizeof(DataType);
+				// Recalculate the message size
+				msg.header.size = msg.size();
 
-        std::memcpy(&data, msg.body.data() + i, sizeof(DataType));
+				// Return the target message so it can be "chained"
+				return msg;
+			}
 
-        msg.body.resize(i);
+			// Pulls any POD-like data form the message buffer
+			template<typename DataType>
+			friend message<T>& operator >> (message<T>& msg, DataType& data)
+			{
+				// Check that the type of the data being pushed is trivially copyable
+				static_assert(std::is_standard_layout<DataType>::value, "Data is too complex to be pulled from vector");
 
-        return msg;
-      }
-    };
-  }
+				// Cache the location towards the end of the vector where the pulled data starts
+				size_t i = msg.body.size() - sizeof(DataType);
+
+				// Physically copy the data from the vector into the user variable
+				std::memcpy(&data, msg.body.data() + i, sizeof(DataType));
+
+				// Shrink the vector to remove read bytes, and reset end position
+				msg.body.resize(i);
+
+				// Recalculate the message size
+				msg.header.size = msg.size();
+
+				// Return the target message so it can be "chained"
+				return msg;
+			}			
+		};
+
+
+		// An "owned" message is identical to a regular message, but it is associated with
+		// a connection. On a server, the owner would be the client that sent the message, 
+		// on a client the owner would be the server.
+
+		// Forward declare the connection
+		template <typename T>
+		class connection;
+
+		template <typename T>
+		struct owned_message
+		{
+			std::shared_ptr<connection<T>> remote = nullptr;
+			message<T> msg;
+
+			// Again, a friendly string maker
+			friend std::ostream& operator<<(std::ostream& os, const owned_message<T>& msg)
+			{
+				os << msg.msg;
+				return os;
+			}
+		};		
+
+		///[OLC_HEADERIFYIER] END "MESSAGE"
+	}
 }
--- src/lib/olc_net.hpp
+++ src/lib/olc_net.hpp
@@ -1,4 +1,8 @@
 #pragma once
 
 #include "net_common.hpp"
+#include "net_tsqueue.hpp"
 #include "net_message.hpp"
+#include "net_client.hpp"
+#include "net_server.hpp"
+#include "net_connection.hpp"
